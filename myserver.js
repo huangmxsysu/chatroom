231a659d3250b699ad52764597842f756ec34e39
mosen <995304572@qq.com>
2017-04-07 01:48:35 +0800

    增加了左边用户信息，用户数量显示，以及增加了相同名字的userid的控制

--- myserver.js
+++ myserver.js
@@ -14,7 +14,6 @@ var multer = require('multer')
 var errorHandler = require('errorhandler')
 
 
-
 var getTime=function(){
   var date = new Date();
   return date.getHours()+":"+date.getMinutes()+":"+date.getSeconds();
@@ -28,8 +27,8 @@ var getColor=function(){
 
 
 
-
-
+var onlineUsers = {};
+var countUsers = 0;
 //WebSocket连接监听
 
 io.on('connection', function (socket) {
@@ -40,10 +39,14 @@ io.on('connection', function (socket) {
   //console.log(socket.request.headers);
   // 构造客户端对象
   var client = {
-    socket:socket,
+    // socket:socket,
     name:false,
     color:getColor()
   }
+
+  var client_flag=null;
+
+
    // 对message事件的监听
   socket.on('message', function(msg){
   	// console.log("===========message==========");
@@ -54,28 +57,44 @@ io.on('connection', function (socket) {
   	var obj = { time:getTime() , color:client.color };
 
   	if(!client.name){
-
-  		client.name = msg;
+      client_flag = msg.userid;
+  		client.name = msg.name;
+          //检查在线列表，如果不在里面就加入
+      if(!onlineUsers.hasOwnProperty(msg.userid)) {
+        onlineUsers[msg.userid] = client;
+        //在线人数+1
+        countUsers++;
+      }
+      console.log(onlineUsers);
   		obj['text'] = client.name;
   		obj['type'] = 'welcome';
   		obj['author'] = '系统';
 
-  		socket.emit('system',obj);
-  		socket.broadcast.emit('system',obj);
+  		socket.emit('system',{onlineUsers:onlineUsers, countUsers:countUsers,obj:obj});
+  		socket.broadcast.emit('system',{onlineUsers:onlineUsers, countUsers:countUsers,obj:obj});
   		console.log(client.name + ' 进入聊天室');
+      var tempclient = io.sockets;
+       
+      
+      console.log(tempclient);
   	}
   	else{
-  		obj['text'] = msg;
+      obj['userid'] = msg.userid;///////////
+  		obj['text'] = msg.name;
   		obj['author'] = client.name;
   		obj['type'] = 'message';
   		socket.emit('message',obj);
   		socket.broadcast.emit('message',obj);
-  		console.log(client.name + ' 说: ' + msg );
+  		console.log(client.name + ' 说: ' + msg.name );
   	}
   });
 
       //监听出退事件
     socket.on('disconnect', function () {  
+      //将退出的用户从在线列表中删除
+      console.log()
+      if(onlineUsers.hasOwnProperty(client_flag)) {
+      //退出用户的信息
       var obj = {
         time:getTime(),
         color:client.color,
@@ -84,10 +103,21 @@ io.on('connection', function (socket) {
         type:'disconnect'
       };
 
+      
+      
+      //删除
+      delete onlineUsers[client_flag];
+      //在线人数-1
+      countUsers--;
+
+
       // 广播用户已退出
-      socket.broadcast.emit('system',obj);
+      socket.broadcast.emit('system',{onlineUsers:onlineUsers,countUsers:countUsers,obj:obj});
       console.log(client.name + ' Disconnect');
-    });
+    }
+
+    }) ;
+
 });
 
 // //express基本配置
@@ -135,7 +165,3 @@ app.get('/', function(req, res){
 server.listen(app.get('port'), function(){
   console.log("Express server listening on port " + app.get('port'));
 });
-
-
-
-
--- public/js/myclient.js
+++ public/js/myclient.js
@@ -3,12 +3,20 @@ $(function () {
     var status = $('#currentName h3');
     var input = $('#input');
     var myName = false;
+    var count = $('#count h3');
+
+    var genUid = function(){
+      return new Date().getTime()+""+Math.floor(Math.random()*899+100);
+    }
+
+    var userid = genUid();
+
 
     var socket = io.connect('http://localhost:3000');
     
 	    //收到server的连接确认
 	    socket.on('open',function(){
-	        status.text('您的昵称');
+	        status.text('您的昵称: []');
             input.attr('placeholder','请输入你的昵称（建议一个字母）回车');
 	    });
 
@@ -20,7 +28,7 @@ $(function () {
                     <div id = "triangle-right"></div>
                 </div>  */
             var template = "";
-            if(myName == obj.author){
+            if(myName == obj.author&&userid == obj.userid){
                 template = '<div id = "myself"><div id = "img">'+obj.author+'</div>';
                 template+= '<div id = "content"><p>'+obj.text+'</p></div>';
                 template+= '<div id = "triangle-right"></div>';
@@ -40,32 +48,47 @@ $(function () {
 
     	socket.on('system',function(obj){
     		//obj{ text author type time color}
+            var userinfo = "";
+            for(item in obj.onlineUsers){
+                if(obj.onlineUsers.hasOwnProperty(item)){
+                    userinfo += '<li style="color:'+obj.onlineUsers[item].color+'">'+obj.onlineUsers[item].name+'</li>';
+
+                }
+            }
+            // $('#userlist')[0].innerHTML='';
+            $('#userlist').empty();
+            $('#userlist').append(userinfo);
+                        
     		var template = "";
-    		if( obj.type == "welcome" ){
-    			if(myName == obj.text){
-    				status.text(myName).css('color', obj.color);
+    		if( obj.obj.type == "welcome" ){
+    			if(myName == obj.obj.text){
+    				status.text('我的昵称: '+ myName).css('color', obj.obj.color);
     			}
-    		    template = '<div id = "system"><p>- - -'+obj.text + ' @ '+obj.time+' 加入聊天室 - - -</p></div>';
+    		    template = '<div id = "system"><p>- - -'+obj.obj.text + ' @ '+obj.obj.time+' 加入聊天室 - - -</p></div>';
     		}
-    		else if(obj.type == 'disconnect'){
-            	template = '<div id = "system"><p>- - -'+obj.text + ' @ '+obj.time+' 离开了聊天室- - -</p></div>';
+    		else if(obj.obj.type == 'disconnect'){
+            	template = '<div id = "system"><p>- - -'+obj.obj.text + ' @ '+obj.obj.time+' 离开了聊天室- - -</p></div>';
         	}
+            count.text('房间在线人数: '+obj.countUsers);
         	message.append(template);
             $('.messagebox').scrollTop($('.messagebox')[0].scrollHeight); 
 
     	});
 
-    	//通过“回车”提交聊天信息
+
+        //通过“回车”提交聊天信息
 	    input.keydown(function(e) {
 	        if (e.keyCode === 13) {
 	            var msg = $(this).val();
 	            if (!msg) return;
-	            socket.emit('message',msg);
+                var msgobj = {name:msg,userid:userid};
+	            socket.emit('message',msgobj);
 	            $(this).val('');
 	            if (myName === false) {
 	                myName = msg;
                     $(this).attr('placeholder','');
 	            }
+
 	        }
 	    });
 
